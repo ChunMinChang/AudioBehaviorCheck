12d294baff1596d593b27cf83b9c511ce88da21f
Chun-Min Chang <chun.m.chang@gmail.com>
2018-07-19 09:52:55 -0700

    Test low-level apis only

--- test_utils.cpp
+++ test_utils.cpp
@@ -1,50 +1,89 @@
 #include "AudioDeviceUtils.h"
+
+#include <cassert>
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
-void printDevicesID(vector<AudioObjectID> ids)
+bool validId(AudioObjectID id)
 {
- for (AudioObjectID id: ids) {
-   cout << id << " : " << AudioDeviceUtils::GetDeviceName(id) << endl;
-   if (AudioDeviceUtils::IsInput(id)) {
-     cout << "\tinput" << endl;
-   }
-   if (AudioDeviceUtils::IsOutput(id)) {
-     cout << "\toutput" << endl;
-   }
- }
- cout << endl;
+  return id != kAudioObjectUnknown;
 }
 
-string getDefaultDeviceName(bool isInput)
+void testGetDefaultDeviceId()
 {
-  AudioObjectID id = AudioDeviceUtils::GetDefaultDeviceId(isInput);
-  return AudioDeviceUtils::GetDeviceName(id);
+  // If we don't have input/output devices, the returned ids must be invalid?
+  // TODO: Find a way to unload all input/outpu devices so we don't have
+  //       default devices and then we can check the behaviors here.
+  //       Find a way to reload all input/outpu devices so we can continue
+  //       tests.
+
+  // If we have default input/output devices, then they must be valid ids.
+  AudioObjectID inId = AudioDeviceUtils::GetDefaultDeviceId(true);
+  assert(validId(inId));
+  AudioObjectID outId = AudioDeviceUtils::GetDefaultDeviceId(false);
+  assert(validId(outId));
 }
 
-void changeDefaultDevice(bool isInput) {
-  cout << "Change default " << (isInput ? "input" : "output")
-       << " device from : " << getDefaultDeviceName(isInput) << " to : "
-       << (AudioDeviceUtils::ChangeDefaultDevice(isInput)
-             ? getDefaultDeviceName(isInput)
-             : " Fail to change!") << endl;
+void testGetDeviceName()
+{
+  // Return empty string if it's a inavlid id.
+  string unknown = AudioDeviceUtils::GetDeviceName(kAudioObjectUnknown);
+  assert(unknown.empty());
+
+  // Return an non-empty string if it's a valid id.
+  AudioObjectID id = AudioDeviceUtils::GetDefaultDeviceId(false);
+  assert(validId(id) == !AudioDeviceUtils::GetDeviceName(id).empty());
+}
+
+void testIsInput()
+{
+  // Return false for an invalid id.
+  assert(!AudioDeviceUtils::IsInput(kAudioObjectUnknown));
+
+  // Return true if we have a valid id.
+  AudioObjectID id = AudioDeviceUtils::GetDefaultDeviceId(true);
+  assert(validId(id) == AudioDeviceUtils::IsInput(id));
+}
+
+void testIsOutput()
+{
+  // Return false for an invalid id.
+  assert(!AudioDeviceUtils::IsOutput(kAudioObjectUnknown));
+
+  // Return true if we have a valid id.
+  AudioObjectID id = AudioDeviceUtils::GetDefaultDeviceId(false);
+  assert(validId(id) == AudioDeviceUtils::IsOutput(id));
+}
+
+void printDeviceInfo(AudioObjectID id)
+{
+  string name = AudioDeviceUtils::GetDeviceName(id);
+  cout << "Device " << id << " : " << name << " >";
+  if (AudioDeviceUtils::IsInput(id)) {
+    cout << " input";
+  }
+  if (AudioDeviceUtils::IsOutput(id)) {
+    cout << " output";
+  }
+  cout << endl;
+}
+
+void testGetAllDeviceIds()
+{
+  vector<AudioObjectID> ids = AudioDeviceUtils::GetAllDeviceIds();
+  for (AudioObjectID id : ids) {
+    printDeviceInfo(id);
+  }
 }
 
 int main()
 {
-  vector<AudioObjectID> ids = AudioDeviceUtils::GetAllDeviceIds();
-  printDevicesID(ids);
-
-  vector<AudioObjectID> ins = AudioDeviceUtils::GetDeviceIds(true);
-  printDevicesID(ins);
-
-  vector<AudioObjectID> outs = AudioDeviceUtils::GetDeviceIds(false);
-  printDevicesID(outs);
-
-  changeDefaultDevice(true);
-  changeDefaultDevice(false);
-
+  testGetDefaultDeviceId();
+  testGetDeviceName();
+  testIsInput();
+  testIsOutput();
+  testGetAllDeviceIds();
   return 0;
 }
