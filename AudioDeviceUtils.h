ec4b3e7fd702c1bc3ffe926aacb394947ee4908c
Chun-Min Chang <chun.m.chang@gmail.com>
2018-07-19 11:18:00 -0700

    add note for coreaudio api

--- AudioDeviceUtils.h
+++ AudioDeviceUtils.h
@@ -17,6 +17,11 @@ public:
   static bool IsInput(AudioObjectID aId);
   static bool IsOutput(AudioObjectID aId);
   static bool SetDefaultDevice(AudioObjectID aId, bool aInput);
+  // NOTE: It's impossible to get input-only or output-only devices in one API
+  //       call. Apple doesn't provide that API(I've comfirmed with my friend
+  //       who works in CoreAudio team.). If you want to get input-only or
+  //       output-only devices, you need to get all the devices first ans then
+  //       check them are input or output ony by one.
   static vector<AudioObjectID> GetAllDeviceIds();
 };
 
